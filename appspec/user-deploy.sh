2a81be2e7763e62d1e0d01b0c2ee39b7db4bb87d
donghonam <rkdl0829@gmail.com>
2023-08-15 18:16:08 +0900

    ci cd 설정 추가

---
+++ appspec/user-deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/user/build
+PROJECT_NAME=user
+
+echo "> Build 파일 복사"
+
+sudo kill $(sudo lsof -t -i:8080)
+
+echo "> 새애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name:$JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+sudo nohup java -jar --spring.config.location=/home/ec2-user/config/application.yaml $JAR_NAME > /home/ubuntu/springlogs/spring.log 2>&1 &
+
+tail -f springlogs/spring.log
+
+echo "> 새 애플리케이션 배포 완료"
